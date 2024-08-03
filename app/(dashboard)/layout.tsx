bb1482702e64280b98d0be20e8dd28d82c3074bc
unknown <blockchainfiras@gmail.com>
2024-08-03 10:45:37 +0100

    Dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Borcelle - Admin Auth",
+  description: "Admin dashboard to manage Borcelle's data",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <UserButton />
+    </div>
+  );
+}
